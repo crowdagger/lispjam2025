927a07284f6b96d93136a2650ebec950496c9126
Lizzie Crowdagger <lizzie@crowdagger.fr>
2025-05-17 01:10:23 +0200

    Add scene

--- if/scene.scm
+++ if/scene.scm
@@ -1,7 +1,30 @@
 (define-module (if scene))
 
-(export define-scene)
+(export define-scene
+        call-scene)
 
+;; define-scene macro
+;;
+;;
+;; ;; What we want
+;; (define-scene some-scene
+;;   (display "Hello!")
+;;   (display "How are you?")
+;;   (choice
+;;    ("Well" (display "Great"))
+;;    ("Not so well" (display "Oh :(")))
+;;   (display "I must go..."))
+;;
+;; ;; transformed to
+;; (define some-scene
+;;   (vector
+;;    (lambda () (display "Hello!"))
+;;    (lambda () (display "How are you?"))
+;;    (lambda () (user-choice '("Well" "Not so well")))
+;;    (lambda (input)   (cond
+;;                       ((equal input 0) (display "Great"))
+;;                       ((equal input 1) (display "Oh :("))
+;;    (else (invalid-input))))))
 
 (define-syntax helper
   (syntax-rules ()
@@ -11,7 +34,7 @@
   (syntax-rules ()
     ((_ name (done ...) ())
      (define name
-       (list done ...)))
+       (vector done ...)))
     ((_ name (done ...) (todo todo* ...))
      (%outer name (done ... (helper todo)) (todo* ...)))
     ))
@@ -19,4 +42,24 @@
 (define-syntax define-scene
   (syntax-rules ()
     ((_ name exp ...)
-     (%outer name () (exp ...)))))
+     (%outer name () (exp ... #f)))))
+
+
+(define* (call-scene scene state #:optional (input #f))
+  "Call the appropriate passage of a scene according to the current state
+
+Returns either a new state, or #f is story has ended."
+  (let ([ret ((vector-ref scene state) input)])
+    (if ret
+        (+ 1 state)
+        (begin
+          (display "Story ended\n")
+          #f))))
+
+
+(define-scene pouet
+  (display "Foo\n")
+  (display "Bar\n")
+  (display "Baz\n"))
+
+(call-scene pouet (call-scene pouet 0))
